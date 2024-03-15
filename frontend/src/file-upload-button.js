a693f48d48ba8c47ebbffd1ef16ebef1e396dab5
geraldDivotion <gerald.van.den.hengel@divotion.com>
2024-03-16 00:09:07 +0100

    response text is hidden when new image is chosen.

--- frontend/src/file-upload-button.js
+++ frontend/src/file-upload-button.js
@@ -24,6 +24,9 @@ export class FileUploadButton extends LitElement {
     this.querySelector('input[type="file"]').showPicker();
     this.querySelector('input[type="file"]').addEventListener('change', (event) => {
         if (event.target.files && event.target.files[0]) {
+          var responseText = document.getElementById('responseText');
+          responseText.classList.add('hidden');
+
           var reader = new FileReader();
           
           reader.onload = (e) => {
@@ -41,19 +44,20 @@ export class FileUploadButton extends LitElement {
             var submitContainer = document.getElementById('submit');
             submitContainer.classList.add('disabled');
 
-            setTimeout((
+            setTimeout(() => {
                 cocoSsd.load().then(model => {
-                  model.detect(img).then(predictions => {
-                    console.log('Predictions: ', predictions);
-                    this.predictions = predictions[0].class
-                    this.requestUpdate();
-                    loading = false;
-                    spinnerContainer.classList.remove('show-spinner');
-                    fileUploadButton.classList.remove('disabled');
-                    submitContainer.classList.remove('disabled');
-                  });
-                })
-            ), 500);
+                    model.detect(img).then(predictions => {
+                        console.log('Predictions: ', predictions);
+                        this.predictions = predictions[0].class
+                        this.requestUpdate();
+                        loading = false;
+                        spinnerContainer.classList.remove('show-spinner');
+                        fileUploadButton.classList.remove('disabled');
+                        submitContainer.classList.remove('disabled');
+                        responseText.classList.remove('hidden');
+                    });
+                });
+            }, 500);
 
           }
           reader.readAsDataURL(event.target.files[0]);
@@ -71,8 +75,8 @@ export class FileUploadButton extends LitElement {
             <div class="spinner" id="spinner"><img src="/src/assets/loading-load.gif"></div>
             <img id="preview" src="${this.imageSrc}" alt="your image" .hidden="${!this.imageSrc}" />
           </div>
-          <div class="file-upload__prediction">
-            <h3 class="response">${this.predictions}</h3>
+          <div class="file-upload__prediction hidden">
+            <h3 class="response hidden" id="responseText">In this image a <span class="response-text">${this.predictions}</span> is found</h3>
           </div>
           <input type="file" id="file-upload" name="${this.name}" value="Upload" hidden>
       </div>
