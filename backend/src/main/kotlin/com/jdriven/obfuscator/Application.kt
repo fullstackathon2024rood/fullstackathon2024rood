4c7aa7833df8185ec7a6ddde25b8f9164289ce8a
Jasper Bogers <joabogers@gmail.com>
2024-03-15 20:13:34 +0100

    /obfuscate endpoint expects multipart form POST with message-text and file, and returns the same file

--- backend/src/main/kotlin/com/jdriven/obfuscator/Application.kt
+++ backend/src/main/kotlin/com/jdriven/obfuscator/Application.kt
@@ -4,17 +4,25 @@ import io.javalin.Javalin
 import io.javalin.util.FileUtil
 
 fun main() {
-
     // Add pages & actions to Javalin
-    val app = Javalin.create()
+    val app =
+        Javalin.create {
+                config ->
+            config.useVirtualThreads // Use virtual threads (based on Java Project Loom)
+        }
 
-    app.post("/obfuscate-me") { ctx ->
+    app.post("/obfuscate") { ctx ->
         ctx.uploadedFiles("files").forEach {
             FileUtil.streamToFile(it.content(), "upload/${it.filename()}")
         }
-        ctx.html("Upload successful")
+
+        val messageText = ctx.formParam("message-text") // ignore for now
+        val fileContent = ctx.uploadedFile("file")?.content()
+
+        // TODO send the file to the obfuscator tool, and fetch the result
+
+        fileContent?.let { ctx.result(fileContent) } ?: ctx.status(500)
     }
 
     app.start(7070)
 }
-
