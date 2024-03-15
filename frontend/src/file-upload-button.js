9562a19c4c26f7edc410a71dc0f1e535d1d5fb3c
quinton.weenink <qweenink@gmail.com>
2024-03-15 21:58:23 +0100

    Tenserflow JS Object Detection

--- frontend/src/file-upload-button.js
+++ frontend/src/file-upload-button.js
@@ -1,10 +1,12 @@
-import {LitElement, css, html} from 'lit';
+import {LitElement, css, html, nothing} from 'lit';
 
 export class FileUploadButton extends LitElement {
   static properties = {
     name: {},
     text: {},
-    fileName: undefined
+    fileName: undefined,
+    imageSrc: {},
+    predictions: {}
   };
 
   createRenderRoot() {
@@ -13,6 +15,26 @@ export class FileUploadButton extends LitElement {
 
   async onClick() {
     this.querySelector('input[type="file"]').showPicker();
+    this.querySelector('input[type="file"]').addEventListener('change', (event) => {
+        if (event.target.files && event.target.files[0]) {
+          var reader = new FileReader();
+          
+          reader.onload = (e) => {
+            this.imageSrc = e.target.result;
+            this.requestUpdate();
+
+            const img = document.getElementById('preview');
+            cocoSsd.load().then(model => {
+            model.detect(img).then(predictions => {
+                console.log('Predictions: ', predictions);
+                this.predictions = predictions[0].class
+                this.requestUpdate();
+              });
+            });
+          }
+          reader.readAsDataURL(event.target.files[0]);
+      }
+    });
   }
 
   // Render the UI as a function of component state
@@ -20,6 +42,8 @@ export class FileUploadButton extends LitElement {
     return html`
       <div class="file-upload">
           <input type="button" value="${this.text}" @click="${this.onClick}">
+          <img id="preview" src="${this.imageSrc}" alt="your image" .hidden="${!this.imageSrc}" />
+          <div>${this.predictions}</div>
           <input type="file" id="file-upload" name="${this.name}" value="Upload" hidden>
       </div>
     `;
