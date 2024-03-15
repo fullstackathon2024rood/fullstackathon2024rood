0f54fecc43a1027e549d606a996ea7ce62863b27
Emil van Galen <emil.van.galen@divotion.com>
2024-03-15 17:19:35 +0100

    Created separate `ui-file-upload-button` and further implemented a (still incomplete) `onSubmit` handler

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,20 +1,24 @@
 import {LitElement, css, html} from 'lit';
+import './file-upload-button.js';
 
 export class App extends LitElement {
-  // static properties = {
-  //   name: {},
-  // };
-
-  constructor() {
-    super();
-  }
-
   createRenderRoot() {
     return this;
   }
 
-  onSubmit() {
-    console.log('!!!');
+  onSubmit(event) {
+    const formData = new FormData(event.target);
+    const files = formData.get('files');
+    const url = URL.createObjectURL(files);
+
+    console.log(url);
+
+    // fetch('/api/medium/article',{
+    //   method: 'POST',
+    //   body: new FormData(event.target)
+    // });
+
+    event.preventDefault();
   }
 
   // Render the UI as a function of component state
@@ -24,14 +28,11 @@ export class App extends LitElement {
 
       <h1>Timeline</h1>
 
-      <form @submit="${this.onSubmit}">
+      <form @submit="${this.onSubmit}" action="#/">
           <label for="messageText">Message</label>
-          <textarea id="messageText"></textarea>
+          <textarea id="messageText" name="message-text"></textarea>
 
-          <div>
-              <label for="file-upload">Upload</label>
-              <input type="file" id="file-upload">
-          </div>
+          <ui-file-upload-button name="files" text="Upload"></ui-file-upload-button>
 
           <button type="submit">submit</button>
       </form>
---
+++ frontend/src/file-upload-button.js
@@ -0,0 +1,28 @@
+import {LitElement, css, html} from 'lit';
+
+export class FileUploadButton extends LitElement {
+  static properties = {
+    name: {},
+    text: {},
+  };
+
+  createRenderRoot() {
+    return this;
+  }
+
+  onClick() {
+    this.querySelector('input[type="file"]').showPicker();
+  }
+
+  // Render the UI as a function of component state
+  render() {
+    return html`
+      <div class="file-upload">
+          <input type="button" value="${this.text}" @click="${this.onClick}">
+          <input type="file" id="file-upload" name="${this.name}" value="Upload" hidden>
+      </div>
+    `;
+  }
+}
+
+customElements.define('ui-file-upload-button', FileUploadButton)
