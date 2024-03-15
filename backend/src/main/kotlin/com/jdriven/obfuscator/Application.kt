eaab4971eb5ddc0495f74d0c2e7560033c233519
Jasper Bogers <joabogers@gmail.com>
2024-03-15 21:23:22 +0100

    Make file input and logging work

--- backend/src/main/kotlin/com/jdriven/obfuscator/Application.kt
+++ backend/src/main/kotlin/com/jdriven/obfuscator/Application.kt
@@ -1,6 +1,9 @@
 package com.jdriven.obfuscator
 
 import io.javalin.Javalin
+import io.javalin.http.UploadedFile
+import org.slf4j.LoggerFactory
+import java.io.InputStream
 
 fun main() {
     // Add pages & actions to Javalin
@@ -10,14 +13,36 @@ fun main() {
             config.useVirtualThreads // Use virtual threads (based on Java Project Loom)
         }
 
+    val obfuscator = Obfuscator()
+
     app.post("/obfuscate") { ctx ->
         val messageText = ctx.formParam("message-text") // ignore for now
-        val fileContent = ctx.uploadedFile("file")?.content()
+        val fileContent = ctx.uploadedFile("file")?.let { obfuscator.extractOriginalFile(it) }
 
-        // TODO send the file to the obfuscator tool, and fetch the result
-
-        fileContent?.let { ctx.result(fileContent) } ?: ctx.status(500)
+        fileContent?.let {
+            obfuscator.obfuscate(fileContent)?.let { obfuscatedFile -> ctx.result(obfuscatedFile) }
+        } ?: {
+            ctx.status(500)
+        }
     }
 
     app.start(7070)
+}
+
+class Obfuscator {
+    fun extractOriginalFile(uploadedFile: UploadedFile): InputStream {
+        logger.info("Found uploaded file ${uploadedFile.filename()}")
+        return uploadedFile.content()
+    }
+
+    fun obfuscate(file: InputStream): InputStream? {
+        // TODO call an external tool to obfuscate the provided file
+        val obfuscatedFile = "/resources/plain_black.jpeg"
+        logger.info("Generating obfuscated file $obfuscatedFile")
+        return ClassLoader.getSystemResourceAsStream(obfuscatedFile)
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(this::class.java)
+    }
 }
