912e5665c15751cb7c941762605f38ab0457cd3d
Jasper Bogers <joabogers@gmail.com>
2024-03-15 20:18:02 +0100

    remove useless filecreation

--- backend/src/main/kotlin/com/jdriven/obfuscator/Application.kt
+++ backend/src/main/kotlin/com/jdriven/obfuscator/Application.kt
@@ -1,7 +1,6 @@
 package com.jdriven.obfuscator
 
 import io.javalin.Javalin
-import io.javalin.util.FileUtil
 
 fun main() {
     // Add pages & actions to Javalin
@@ -12,10 +11,6 @@ fun main() {
         }
 
     app.post("/obfuscate") { ctx ->
-        ctx.uploadedFiles("files").forEach {
-            FileUtil.streamToFile(it.content(), "upload/${it.filename()}")
-        }
-
         val messageText = ctx.formParam("message-text") // ignore for now
         val fileContent = ctx.uploadedFile("file")?.content()
 
