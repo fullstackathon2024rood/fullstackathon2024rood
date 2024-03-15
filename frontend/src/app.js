750db6b2bb6c2766dd2a452247e001126aec0931
Emil van Galen <emil.van.galen@divotion.com>
2024-03-15 18:49:44 +0100

    Mock Service Worker toegevoegd met HTTP GET /messages mock, basic <ui-timeline-message> mock die responses van HTTP mock gebruikt

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,8 +1,24 @@
-import {LitElement, css, html} from 'lit';
+import { LitElement, css, html } from 'lit';
 import './file-upload-button.js';
+import './timeline-message.js';
 import {toggleNightmare} from './jakob-nielsen-nightmare.js';
 
 export class App extends LitElement {
+  messages = [];
+
+  constructor() {
+    super();
+
+    setTimeout(() => {
+      fetch('/messages', {method: 'GET'})
+        .then(response => response.json())
+        .then(responseJson => {
+          this.messages = responseJson;
+          this.requestUpdate();
+        });
+    }, 1000);
+  }
+
   createRenderRoot() {
     return this;
   }
@@ -25,18 +41,20 @@ export class App extends LitElement {
   // Render the UI as a function of component state
   render() {
     return html`
-      
-      <div class="centerContainer">
+        <div class="centerContainer">
         <div class="logoContainer"><img src="./src/assets/facebook.svg" id="logoImage" @click="${toggleNightmare}"></div>
-  
+
         <h1>Timeline</h1>
-  
+
+        ${this.messages?.map(message => 
+                html`<ui-timeline-message .messageText=${message.messageText} imageUrl="${message.imageUrl}"></ui-timeline-message>`)}
+
         <form @submit="${this.onSubmit}" action="#/">
             <label for="messageText">Message</label>
             <textarea id="messageText" name="message-text"></textarea>
-  
-            <ui-file-upload-button name="files" text="Upload"></ui-file-upload-button>
-  
+
+            <ui-file-upload-button name="file" text="Upload"></ui-file-upload-button>
+
             <button type="submit">submit</button>
         </form>
 
