9a16c898ae9cca119b25d09955d53cae944574da
geraldDivotion <gerald.van.den.hengel@divotion.com>
2024-03-16 00:17:02 +0100

    only one change event... And not after every image chosen

--- frontend/src/file-upload-button.js
+++ frontend/src/file-upload-button.js
@@ -17,52 +17,57 @@ export class FileUploadButton extends LitElement {
     return this;
   }
 
+    async firstUpdated() {
+        this.querySelector('input[type="file"]').addEventListener('change', (event) => {
+            console.log('change event')
+            if (event.target.files && event.target.files[0]) {
+                var responseText = document.getElementById('responseText');
+                responseText.classList.add('hidden');
+
+                var reader = new FileReader();
+
+                reader.onload = (e) => {
+                    this.imageSrc = e.target.result;
+
+                    loading = true;
+                    this.requestUpdate();
+
+                    const img = document.getElementById('preview');
+
+                    var spinnerContainer = document.getElementById('spinner');
+                    spinnerContainer.classList.add('show-spinner');
+                    var fileUploadButton = document.getElementById('file-upload-button');
+                    fileUploadButton.classList.add('disabled');
+                    var submitContainer = document.getElementById('submit');
+                    submitContainer.classList.add('disabled');
+
+                    setTimeout(() => {
+                        cocoSsd.load().then(model => {
+                            model.detect(img).then(predictions => {
+                                console.log('Predictions: ', predictions);
+                                this.predictions = predictions[0].class
+                                this.requestUpdate();
+                                loading = false;
+                                spinnerContainer.classList.remove('show-spinner');
+                                fileUploadButton.classList.remove('disabled');
+                                submitContainer.classList.remove('disabled');
+                                responseText.classList.remove('hidden');
+                            });
+                        });
+                    }, 500);
+
+                }
+                reader.readAsDataURL(event.target.files[0]);
+            }
+        });
+    }
+
   async onClick() {
     if(loading) {
       return;
     }
     this.querySelector('input[type="file"]').showPicker();
-    this.querySelector('input[type="file"]').addEventListener('change', (event) => {
-        if (event.target.files && event.target.files[0]) {
-          var responseText = document.getElementById('responseText');
-          responseText.classList.add('hidden');
 
-          var reader = new FileReader();
-          
-          reader.onload = (e) => {
-            this.imageSrc = e.target.result;
-
-            loading = true;
-            this.requestUpdate();
-
-            const img = document.getElementById('preview');
-
-            var spinnerContainer = document.getElementById('spinner');
-            spinnerContainer.classList.add('show-spinner');
-            var fileUploadButton = document.getElementById('file-upload-button');
-            fileUploadButton.classList.add('disabled');
-            var submitContainer = document.getElementById('submit');
-            submitContainer.classList.add('disabled');
-
-            setTimeout(() => {
-                cocoSsd.load().then(model => {
-                    model.detect(img).then(predictions => {
-                        console.log('Predictions: ', predictions);
-                        this.predictions = predictions[0].class
-                        this.requestUpdate();
-                        loading = false;
-                        spinnerContainer.classList.remove('show-spinner');
-                        fileUploadButton.classList.remove('disabled');
-                        submitContainer.classList.remove('disabled');
-                        responseText.classList.remove('hidden');
-                    });
-                });
-            }, 500);
-
-          }
-          reader.readAsDataURL(event.target.files[0]);
-      }
-    });
   }
 
   // Render the UI as a function of component state
